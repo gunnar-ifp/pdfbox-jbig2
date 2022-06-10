92d97f70bd2d7bddec9859a60be39db569035fce
Gunnar Brand <gunnar.brand@interface-projects.de>
2022-06-10 12:34:22 +0200

    Compact ArithmeticIntegerDecoder
    uses optimized bit shifting

--- src/main/java/org/apache/pdfbox/jbig2/decoder/arithmetic/ArithmeticIntegerDecoder.java
+++ src/main/java/org/apache/pdfbox/jbig2/decoder/arithmetic/ArithmeticIntegerDecoder.java
@@ -24,6 +24,16 @@ import java.io.IOException;
  */
 public class ArithmeticIntegerDecoder
 {
+    /** List of offset, bitsToRead pairs. */
+    private final static int[] VALUES = {
+        0    << 16 | 2,
+        4    << 16 | 4,
+        20   << 16 | 6,
+        84   << 16 | 8,
+        340  << 16 | 12,
+        4436 << 16 | 32
+    };
+    
 
     private final ArithmeticDecoder decoder;
 
@@ -42,105 +52,34 @@ public class ArithmeticIntegerDecoder
      */
     public long decode(CX cxIAx) throws IOException
     {
-        int v = 0;
-        int d, s;
-
-        int bitsToRead;
-        int offset;
-
         if (cxIAx == null)
         {
             cxIAx = new CX(512);
         }
 
-        int prev = 1;
-
-        s = decoder.decode(cxIAx, prev);
-        prev = setPrev(prev, s);
-
-        d = decoder.decode(cxIAx, prev);
-        prev = setPrev(prev, d);
-
-        if (d == 1)
+        final int s = decoder.decode(cxIAx, 1);
+        int prev = 2 | s;
+        
+        int offset = 0;
+        do
         {
-            d = decoder.decode(cxIAx, prev);
-            prev = setPrev(prev, d);
-
-            if (d == 1)
-            {
-                d = decoder.decode(cxIAx, prev);
-                prev = setPrev(prev, d);
-
-                if (d == 1)
-                {
-                    d = decoder.decode(cxIAx, prev);
-                    prev = setPrev(prev, d);
-
-                    if (d == 1)
-                    {
-                        d = decoder.decode(cxIAx, prev);
-                        prev = setPrev(prev, d);
-
-                        if (d == 1)
-                        {
-                            bitsToRead = 32;
-                            offset = 4436;
-                        }
-                        else
-                        {
-                            bitsToRead = 12;
-                            offset = 340;
-                        }
-                    }
-                    else
-                    {
-                        bitsToRead = 8;
-                        offset = 84;
-                    }
-                }
-                else
-                {
-                    bitsToRead = 6;
-                    offset = 20;
-                }
-            }
-            else
-            {
-                bitsToRead = 4;
-                offset = 4;
-            }
+            int d = decoder.decode(cxIAx, prev);
+            prev = prev << 1 | d;
+            if ( d==0 ) break;
         }
-        else
+        while ( ++offset < 5 );
+        offset = VALUES[offset];
+        
+        int v = decoder.decode(cxIAx, prev);
+        for ( int d = v, count = (short)offset; --count!=0; )
         {
-            bitsToRead = 2;
-            offset = 0;
-        }
-
-        for (int i = 0; i < bitsToRead; i++)
-        {
+            prev = prev << 1 & 0x1ff | prev & 0x100 | d;
             d = decoder.decode(cxIAx, prev);
-            prev = setPrev(prev, d);
             v = (v << 1) | d;
         }
-
-        v += offset;
-
-        if (s == 0)
-        {
-            return v;
-        }
-        else if (s == 1 && v > 0)
-        {
-            return -v;
-        }
-
-        return Long.MAX_VALUE;
-    }
-
-    private static int setPrev(int prev, int bit)
-    {
-//      return (prev << 1 | bit | prev & 0x100) & 0x1ff;
-        return (prev & 0xff) << 1 | prev & 0x100 | bit;
+        v += offset >>> 16;
+        
+        return s == 0 ? v : v > 0 ? -v : Long.MAX_VALUE;
     }
 
     /**
